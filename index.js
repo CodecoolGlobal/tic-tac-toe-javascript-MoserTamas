a6923b4c36f812ab037bb4e463caae94a2976427
Szücs Bence Péter <szucs.bence.peter@gmail.com>
2022-11-29 13:18:42 +0100

    uptodate?

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function setGameMode(selectedValue) {
 
     setHTMLvisibilityForInputGameMode(false);
     setHTMLvisibilityForInputHumanCoordinates(true);
-    setHTMLvisibilityForInputAiCoordinatesInput(false);
+    setHTMLvisibilityForInputAiCoordinatesInput(true);
     setHTMLvisibilityForButtonLabeledReset(true);
     displayMessage("Player X's turn");
 }
@@ -29,7 +29,7 @@ function setGameMode(selectedValue) {
 // paramerter: input - the content of the input box
 function processHumanCoordinate(input) {
     console.log(`'processHumanCoordinate('${input}')`);
-    
+
     if (gameTurn % 2 === 0) {
         currentPlayer = 'diamond';
         displayMessage("Player Pets turn");
@@ -39,28 +39,26 @@ function processHumanCoordinate(input) {
     }
 
     let coordinates = extractCoordinates(input);
-    
+
     // üres cella ?
-    if(board[coordinates.x][coordinates.y] === ''){
+    if (board[coordinates.x][coordinates.y] === '') {
         board[coordinates.x][coordinates.y] = currentPlayer;
-        
+
         const winningPlayer = getWinningPlayer(board);
         if (winningPlayer) {
             displayMessage(`Player ${currentPlayer} has won !`);
         }
-        
+
         gameTurn += 1;
         displayBoard(board);
-    
+
         // TODO: add a message stating either
         // Player X's turn
         // Player O's turn
         // It's a tie
         // Player X won 
         // Player O won 
-        if (selectedValue === 'human-ai') {
-            setHTMLvisibilityForInputHumanCoordinates(false)
-        }
+
         // TODO: add conditions to hide the coordinates screen for 
         // the human player & show for the button to generate AI 
         // coordinates
@@ -76,8 +74,12 @@ function processHumanCoordinate(input) {
 // this function is called whenever the user presses
 // the button labeled `Generate AI coordinates`
 function processAICoordinate() {
-    console.log(`processAICoordinate()`);
+    // console.log(`processAICoordinate()`);
+    // if (selectedValue === 'human-ai') {
+    //     setHTMLvisibilityForInputHumanCoordinates(false);
+    //     setHTMLvisibilityForInputAiCoordinatesInput(true);
 
+    // }
 }
 
 // this function is called when the user clicks on 
@@ -96,49 +98,49 @@ function resetGame() {
 
 
 
-    
+
 // this function should change from A1..C3 to coordinates
 // that are present in the `board` global variable
 function extractCoordinates(input) {
-    
-    
+
+
     // const colIndexes = 'ABCDEF';
     // const row = (colIndexes.split('')).indexOf(input[0].toUpper());
     // const col = parseInt(input[1]) - 1;
-    
+
     let coordinates = {
         x: 0,
         y: 0
     }
-    
-    if(input == 'a1'){
+
+    if (input == 'a1') {
         coordinates.x = 0;
-        coordinates.y = 0; 
-    }else if(input == 'a2'){
+        coordinates.y = 0;
+    } else if (input == 'a2') {
         coordinates.x = 0;
         coordinates.y = 1;
-    }else if(input == 'a3'){
+    } else if (input == 'a3') {
         coordinates.x = 0;
-        coordinates.y = 2; 
-    }else if(input == 'b1'){
+        coordinates.y = 2;
+    } else if (input == 'b1') {
         coordinates.x = 1;
-        coordinates.y = 0; 
-    }else if(input == 'b2'){
+        coordinates.y = 0;
+    } else if (input == 'b2') {
         coordinates.x = 1;
-        coordinates.y = 1; 
-    }else if(input == 'b3'){
+        coordinates.y = 1;
+    } else if (input == 'b3') {
         coordinates.x = 1;
-        coordinates.y = 2; 
-    }else if(input == 'c1'){
+        coordinates.y = 2;
+    } else if (input == 'c1') {
         coordinates.x = 2;
-        coordinates.y = 0; 
-    }else if(input == 'c2'){
+        coordinates.y = 0;
+    } else if (input == 'c2') {
         coordinates.x = 2;
-        coordinates.y = 1; 
-    }else if(input == 'c3'){
+        coordinates.y = 1;
+    } else if (input == 'c3') {
         coordinates.x = 2;
-        coordinates.y = 2; 
-    }else {
+        coordinates.y = 2;
+    } else {
         displayMessage("Invalid coordinate entered");
         coordinates.x = 4;
         coordinates.y = 4;
@@ -154,6 +156,8 @@ function extractCoordinates(input) {
 // based on interpreting the values in the board variable
 function getWinningPlayer(board) {
     
+
+
 
 
     return undefined;
