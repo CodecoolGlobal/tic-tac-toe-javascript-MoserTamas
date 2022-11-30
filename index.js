1ba6a95dee5e2cf1a039357e88e38141a2e3cf74
Bence Bodi <bence309@hotmail.com>
2022-11-30 15:54:12 +0100

    updated

--- index.js
+++ index.js
@@ -274,19 +274,18 @@ function getWinningPlayer(board) {
     } else if(board[2][0] == "pets" && board[1][1] == "pets" && board[0][2] == "pets" ){
         displayMessage("Pets won!");
         setHTMLvisibilityForInputHumanCoordinates(false)
-        setHTMLvisibilityForInputAiCoordinatesInput(false)   
+        setHTMLvisibilityForInputAiCoordinatesInput(false)   }
    
 
-if (board[0][0] !== "" && board[0][1] !== "" && board[0][2] !== "" &&
-    board[1][0] !== "" && board[1][1] !== "" && board[1][2] !== "" &&
-    board[2][0] !== "" && board[2][1] !== "" && board[2][2] !== ""){
+if (board[0][0] != "" && board[0][1] != "" && board[0][2] != "" &&
+    board[1][0] != "" && board[1][1] != "" && board[1][2] != "" &&
+    board[2][0] != "" && board[2][1] != "" && board[2][2] != ""){
         displayMessage("It's a tie!");
     setHTMLvisibilityForInputHumanCoordinates(false)
     setHTMLvisibilityForInputAiCoordinatesInput(false)
     }
 }
 
-}
 
 
     
